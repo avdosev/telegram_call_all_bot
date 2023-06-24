ef18a03f304bdb7a8a33cecff9a8d2cca8adca5c
avdosev <avdosev1@yandex.ru>
2023-06-24 18:49:40 +0000

    fix bot restart

--- updater.sh
+++ updater.sh
@@ -1,2 +1,8 @@
-git pull
-systemctl restart callbot.service
+# Perform the git pull command and capture the output
+output=$(git pull)
+
+# Check if the output does not contain the string "Already up to date"
+if [[ $output != *"Already up to date"* ]]; then
+    echo "Git pull updated the repository."
+    systemctl restart callbot.service
+fi
