af2c00c419ff6e07dc50943bf142e34cf85f53d4
Daniil Dudkin <unterumarmung@yandex.ru>
2024-02-07 12:06:57 +0300

    Fix reloads every 5 mins (#5)

--- updater.sh
+++ updater.sh
@@ -1,8 +1,24 @@
-# Perform the git pull command and capture the output
-output=$(git pull)
+#!/bin/bash
 
-# Check if the output does not contain the string "Already up to date"
-if [[ $output != *"Already up to date"* ]]; then
-    echo "Git pull updated the repository."
-    systemctl restart callbot.service
+# Fetch the latest changes from origin without merging them
+git fetch origin
+
+# Compare the local master branch with the remote master branch
+LOCAL=$(git rev-parse master)
+REMOTE=$(git rev-parse origin/master)
+
+if [ "$LOCAL" != "$REMOTE" ]; then
+    # Check if local is behind remote
+    BASE=$(git merge-base master origin/master)
+    if [ "$LOCAL" = "$BASE" ]; then
+        echo "Local master is behind the remote master, updating and restarting callbot.service"
+        # Pull the remote master into local master
+        git pull origin master
+        # Restart the callbot.service
+        systemctl restart callbot.service
+    else
+        echo "Local master has diverged from remote master or is ahead. Manual intervention required."
+    fi
+else
+    echo "Local master is up to date with the remote master."
 fi
