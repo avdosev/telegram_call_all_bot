cdde12e86580e7b154e4dac50eecd5cdf9a965eb
Nikita Avdosev <avdosev1@yandex.ru>
2024-02-11 20:55:23 +0300

    add support whisper-fast

---
+++ whisper_fast_voice.py
@@ -0,0 +1,30 @@
+from faster_whisper import WhisperModel
+import concurrent.futures
+import time
+import asyncio
+import logging
+
+logger = logging.getLogger('whisper-fast')
+
+executor = concurrent.futures.ThreadPoolExecutor(max_workers=1, thread_name_prefix='whisper-faster')
+
+model = WhisperModel('small', device="cpu", compute_type="int8")
+
+def transcribe_sync(voice, file_id):
+    start = time.time()
+    segments, info = model.transcribe(voice, beam_size=5)
+
+    logger.debug("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+    text = ' '.join([segment.text for segment in segments])
+    
+    end = time.time()
+    
+    logger.info(f'Time: {end-start}, Id: {file_id}')
+    return text
+
+
+lock = asyncio.Lock()
+async def transcribe(voice, *args):
+    loop = asyncio.get_running_loop()
+    async with lock:    
+        return await loop.run_in_executor(executor, transcribe_sync, voice, *args)
